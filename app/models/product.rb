b23b3bea8c44af4a43ed1bcaf97c67c148ab7b53
Nghi Pham <kalickvn2@gmail.com>
2016-11-07 10:00:26 +0700

    #change association follow updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_many :product_details,class_name: "ProductPriceDetail", primary_key: "site_product_id",foreign_key: "site_product_id"
+  has_many :product_details,class_name: "ProductPriceDetail"
   
   extend FriendlyId
   friendly_id :slug_candidates, use: [:slugged, :finders]
