af8f4a1d3cfb1422b82f2ac6314a8b8909b53a5c
Nghi Pham <kalickvn2@gmail.com>
2016-11-07 09:39:43 +0700

    #Add footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  layout "static_layout",:only [:about_us]
   protect_from_forgery with: :exception
   def index
     # redirect_to new_user_session_path
@@ -10,4 +11,37 @@ class HomeController < ApplicationController
   	render partial: "addresses/state_select"
   end
   
+  def about_us
+  	
+  end
+
+
+  def faq
+  	
+  end
+
+  def domestic_shipping_return
+  	
+  end
+
+  def international_shipping_return
+  	
+  end
+
+  def privacy_policy
+  	
+  end
+
+  def safe_shopping_guarantee
+  	
+  end
+
+  def secure_shopping
+  	
+  end
+
+  def term_of_use
+  	
+  end
+
 end
